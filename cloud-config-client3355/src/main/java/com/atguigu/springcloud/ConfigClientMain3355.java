5307fcad68b7b048683206c9d0721e1f8ed6494b
cuizhigang <cuizg2015fly@163.com>
2022-07-18 00:25:12 +0800

    to be continued.

--- cloud-config-client3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -6,6 +6,17 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * create by 携山超 on 2020/6/2
+ *
+ * 什么是总线？
+ *
+ * 在微服务架构的系统中，通常会使用轻量级的消息代理来构建一个共用的消息主题，并让系统中所有的微服务实例都连接上来。
+ * 由于该主题中产生的消息会被所有实例监听和消费，所以称它为消息总线。在总线上的各个实例，都可以方便地广播一些需要
+ * 让其他连接在该主题上的实例都知道的消息。
+ *
+ * 基本原理
+ *
+ * ConfigClient 实例都监听 MQ 中同一个 topic(默认是 springCloudBus)。当一个服务刷新数据的时候，它会把这个信息
+ * 放到 topic 中，这样其他监听这个 topic 的服务就能得到通知，然后去更新自身的配置。
  */
 @SpringBootApplication
 @EnableEurekaClient
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMain8801.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMain8801.java
@@ -9,6 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/9/1 16:25
  * @Description StreamMQMain8801
  * @Version 1.0
+ *
+ * 什么是 Spring Cloud Stream ？
+ *
+ * 官方定义 Spring Cloud Stream 是一个构建消息驱动微服务的框架。
+ *
+ * 应用程序通过 inputs 或者 outputs 来与 Spring Cloud Stream 中的 binder 对象交互。通过配置来 binding（绑定），而 Spring Cloud Stream
+ * 的 binder 对象负责与消息中间件交互。所以，我们只需要搞清楚如何与 Spring Cloud Stream 交互就可以方便地使用消息驱动的方式。
+ *
+ * 通过使用 Spring Integration 来连接消息代理中间件以实现消息事件驱动。Spring Cloud Stream 为一些供应商的消息中间件产品提供了个性化的自动化配置实现，引用了发布-订阅、消费组、
+ * 分区的三个核心概念。
+ *
+ * 目前仅支持 RabbitMQ、Kafka
  */
 @SpringBootApplication
 public class StreamMQMain8801 {
